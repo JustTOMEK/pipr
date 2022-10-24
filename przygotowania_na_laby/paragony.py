933d7603d44606f5e77defb236bfc0f877179942
Tomasz Truszkowski <trunio31@gmail.com>
2022-10-24 22:37:28 +0200

    imlemented print receipt

--- przygotowania_na_laby/paragony.py
+++ przygotowania_na_laby/paragony.py
@@ -1,3 +1,6 @@
+from datetime import datetime
+
+
 def split_price(price):
     price_zl = price // 100
     price_gr = price % 100
@@ -11,7 +14,7 @@ def format_price(price):
 
 def get_description(name, price):
     price_parts = split_price(price)
-    return f'Price of {name} is {price_parts[0]}.{price_parts[1]:02}'
+    return f'{name}\t{price_parts[0]}.{price_parts[1]:02}'
 
 
 def print_description(name, price):
@@ -32,10 +35,24 @@ def get_total_price(receipt):
     return total_price
 
 
+def print_receipt(receipt):
+    now = datetime.now()
+    dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
+    print(dt_string)
+    product_count = 1
+    for name, price in receipt:
+        price = format_price(price)
+        print(f' {product_count:2}. {name:19}{price:>6}')
+        product_count += 1
+    print('-' * 30)
+    total_price = get_total_price(receipt)
+    formatted_value = format_price(total_price)
+    print(f'TOTAL:{formatted_value:>24}')
+
+
 my_receipt = [
     ('Stanik', 9283),
     ('Majtki', 432),
     ('Mleko', 321)]
 
-
-print(format_price(get_total_price(my_receipt)))
+print_receipt(my_receipt)
