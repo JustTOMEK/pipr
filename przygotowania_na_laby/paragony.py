15caee8e87dacb79691823b64690b890b25025ea
Tomasz Truszkowski <trunio31@gmail.com>
2022-11-01 21:22:55 +0100

    implemented a tax_vale to print_receipt

--- przygotowania_na_laby/paragony.py
+++ przygotowania_na_laby/paragony.py
@@ -31,17 +31,21 @@ def get_total_price(receipt):
 
 def print_receipt(receipt):
     now = datetime.now()
-    dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
+    dt_string = now.strftime("%d/%m/%Y")
     print(dt_string)
     product_count = 1
+    tax_value = 0
     for name, price in receipt:
+        tax_value += price * get_tax_value(get_tax_group(name))
         price = format_price(price)
-        print(f'{product_count:2}.{name:16} {price:>6} {get_tax_group(name)}')
+        print(f'{product_count}. {name:10} {price:>13} {get_tax_group(name)}')
         product_count += 1
     print('-' * 30)
     total_price = get_total_price(receipt)
     formatted_value = format_price(total_price)
+    print(f'TAX:{format_price(tax_value//100):>23}')
     print(f'TOTAL:{formatted_value:>21}')
+    print(f'TOTAL+TAX:{format_price(tax_value//100+total_price):>17}')
 
 
 def get_tax_group(product):
@@ -55,11 +59,16 @@ def get_tax_group(product):
         return 'E'
 
 
+def get_tax_value(tax_group):
+    slownik = {'A': 12, 'B': 8, 'E': 22}
+    return slownik[tax_group]
+
+
 def get_product():
     product_prices = {
-        'Stanik': 9283,
-        'Majtki': 432,
-        'Mleko': 321
+        'Stanik': 92883,
+        'Majtki': 4382,
+        'Mleko': 3281
     }
     name = input('Give a product name: ')
     quantity = int(input('Give a product quantity: '))
@@ -67,12 +76,8 @@ def get_product():
 
 
 my_receipt = [
-    ('Stanik', 9283),
-    ('Majtki', 432),
-    ('Mleko', 321)]
+    ('Stanik', 97283),
+    ('Majtki', 4732),
+    ('Mleko', 3271)]
 
 print_receipt(my_receipt)
-
-print(get_tax_group('Oranges'))
-
-print(get_product())
