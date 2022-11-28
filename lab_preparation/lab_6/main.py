5a1c1dbc1c08ad37305bcbb3be7fd9f75f1ea3d6
Tomasz Truszkowski <trunio31@gmail.com>
2022-11-28 23:08:58 +0100

    added new files and deleted some

---
+++ lab_preparation/lab_6/main.py
@@ -0,0 +1,11 @@
+from definitions import Object,  Person
+
+
+jurek = Person('Jurek', 'Ogórek')
+karolina = Person('Karolina', 'Malina')
+
+print(jurek.introduce())
+
+strawberry = Object()
+strawberry.color = 'red'
+strawberry.name = 'strawberry'
